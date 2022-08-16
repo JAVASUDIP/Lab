78a048ff9a21f471e6b54c188794650a3cb7208e
JAVASUDIP <110465798+JAVASUDIP@users.noreply.github.com>
2022-08-16 12:02:39 +0530

    Add files via upload

---
+++ Hw7.java
@@ -0,0 +1,24 @@
+// Q.2 The Swapping two variables.
+
+
+
+class Hw7{
+
+public static void main(String[]args){
+
+
+ int a,b,temp;
+
+a = 105;
+b = 227;
+
+System.out.println("Before sweep a,b:" +a+","+b);
+
+ temp = a;
+ a = b ;
+ b = temp;
+
+System.out.println("After sweep a,b:" +a+","+b);
+    }
+
+}
